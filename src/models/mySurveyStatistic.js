2f03d7beb989b52a4402ef3fd084a9cdef270f8e
RicardoHzx <1599583168@qq.com>
2019-07-05 10:56:50 +0800

    胡芷璇：课调统计部分

--- src/models/mySurveyStatistic.js
+++ src/models/mySurveyStatistic.js
@@ -1,47 +1,71 @@
-import { query } from '@/services/mySurveyStatistic';
-
+import {
+  fetchMySurveyStatistics,
+  fetchSurveyMonth,
+  fetchSurveyDetails,
+} from '@/services/mySurveyStatistic';
 
 const mySurveyStatisticModel = {
-    namespace: 'mySurveyStatistic',
-    state: {
-      mySurveyStatistics: [],
-      survey: {
-          average: '',
-          course: { name: '' },
-          clazzVM: { grade: { name: '' }, charge: {} },
-          user: { nickname: '' },
-          qnVM: { name: '' },
-        },
-    },
-    effects: {
-      //获取所有信息
-      *fetchMySurveyStatistics(_, { call, put }) {
-        const response = yield call(query);
-        yield put({
-          type: 'reloadMySurveyStatistics',
-          payload: response.data,
-        });
+  namespace: 'mySurveyStatistic',
+  state: {
+    mySurveyStatistics: [],
+    survey: {
+      surveyVM: {
+        average: '',
+        course: { name: '' },
+        clazzVM: { grade: { name: '' }, charge: {} },
+        user: { nickname: '' },
+        qnVM: { name: '' },
       },
-      *setSurvey(action, { call, put }) {
-          yield put({ type: 'setSurveys', payload: action.payload });
+      // answers:{content:''}
+      answers: [''],
     },
+  },
+  effects: {
+    //获取所有信息
+    *fetchMySurveyStatistics(_, { call, put }) {
+      const response = yield call(fetchMySurveyStatistics);
+      yield put({
+        type: 'reloadMySurveyStatistics',
+        payload: response.data.data,
+      });
     },
-    reducers: {
-      // 更新状态中的mySurveyStatistic
-      reloadMySurveyStatistics(state, action) {
-        return {
-          ...state,
-          mySurveyStatistics: action.payload,
-        };
-      },
-      setSurveys(state, action) {
-          return {
-            ...state,
-            survey: action.payload,
-          };
-        },
-      },
-  };
-  
-  export default mySurveyStatisticModel;
-  
+    *fetchSurveyMonth(_, { call, put }) {
+      const response = yield call(fetchSurveyMonth, { month: _.payload });
+      yield put({
+        type: 'reloadSurveyMonth',
+        payload: response.data.data,
+      });
+    },
+    *fetchSurveyDetails(_, { call, put }) {
+      const response = yield call(fetchSurveyDetails, { id: _.payload });
+      console.log('response', response);
+      yield put({
+        type: 'reloadSurveyDetails',
+        payload: response.data,
+      });
+    },
+  },
+  reducers: {
+    // 更新状态中的mySurveyStatistic
+    reloadMySurveyStatistics(state, action) {
+      return {
+        ...state,
+        mySurveyStatistics: action.payload,
+      };
+    },
+    reloadSurveyDetails(state, action) {
+      return {
+        ...state,
+        survey: action.payload,
+      };
+    },
+    reloadSurveyMonth(state, action) {
+      return {
+        ...state,
+        statistic: action.payload,
+      };
+    },
+  },
+};
+
+export default mySurveyStatisticModel;
--- src/services/mySurveyStatistic.js
+++ src/services/mySurveyStatistic.js
@@ -1,6 +1,24 @@
 import request from '@/utils/request';
+import { func } from 'prop-types';
 
 // 封装所有异步请求
-export async function query() {
-  return request('/api/manager/survey/findAllSurveyVM');
+export async function fetchMySurveyStatistics() {
+  const params = { page: 0, pageSize: 10, statuses: ['审核通过'] };
+  return request('/api/manager/survey/querySurvey', {
+    method: 'post',
+    params: params,
+    getResponse: true,
+  });
+}
+
+export async function fetchSurveyDetails(id) {
+  return request('http://134.175.154.93:9999/teacher/previewSurvey', { params: id });
+}
+
+export async function fetchSurveyMonth(survey) {
+  return request('api/manager/survey/statisticsByMonth', {
+    method: 'post',
+    params: survey,
+    getResponse: true,
+  });
 }
